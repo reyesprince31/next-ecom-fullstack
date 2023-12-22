f7e021ef8629a1999ffdae3e84b01845a9bcccb2
Prince Reyes <krayzerprince@gmail.com>
2023-12-22 14:14:39 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hello Admin Dashboard
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+}
